e5ff92e89977a259622d6d9eb78cfb1688b6921a
Marc Lavergne <mlavergn@gmail.com>
2024-03-09 23:58:53 -0500

    Preact table

--- preact/app.js
+++ preact/app.js
@@ -1,6 +1,44 @@
-import { h, render } from 'https://esm.sh/preact';
+// import { h, Component, render } from 'https://esm.sh/preact';
+import { html, render, Component } from 'https://esm.sh/htm/preact/standalone'
+
+
+// Table
+export class UserTable extends Component {
+    draw(data) {
+        return html`
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    ${data.users.map((user) =>
+                        html`
+                            <tr>
+                            <td>${user.id}</td>
+                            <td>${user.name}</td>
+                            <td>${user.email}</td>
+                            </tr>
+                        `
+                    )}
+                </tbody>
+            </table>
+        `;
+    }
+}
+
+const state = {
+    users: [
+        { "id": 1, "name": "John Doe", "email": "john.doe@example.com" },
+        { "id": 2, "name": "Jane Doe", "email": "jane.doe@example.com" }
+    ]
+};
 
 // Create your app
-const app = h('h1', null, 'Hello World!');
+// const app = h('h1', null, 'Hello World!');
+// render(app, document.body);
 
-render(app, document.body);
+render(html`<${new UserTable().draw} users=${state.users} />`, document.body);
